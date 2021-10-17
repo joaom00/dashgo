c9ac8014db23b5862c08e8fb39912d42db286283
João Pedro <joaopsilvamagalhaes@gmail.com>
2021-10-17 11:36:29 -0300

    feat: responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,20 +1,22 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react'
 
-export function Profile() {
+type ProfileProps = {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>João Pedro</Text>
-        <Text color="gray.300" fontSize="small">
-          joaomljcanal@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>João Pedro</Text>
+          <Text color="gray.300" fontSize="small">
+            joaomljcanal@gmail.com
+          </Text>
+        </Box>
+      )}
 
-      <Avatar
-        size="md"
-        name="Diego Fernandes"
-        src="https://github.com/joaom00.png"
-      />
+      <Avatar size="md" name="Diego Fernandes" src="https://github.com/joaom00.png" />
     </Flex>
   )
 }

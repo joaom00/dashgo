fc725752e772e7cf9e294f07b19ee0c88e7119d6
João Pedro <joaopsilvamagalhaes@gmail.com>
2021-10-22 20:00:33 -0300

    feat: useUsers hook

---
+++ src/services/hooks/useUsers.ts
@@ -0,0 +1,37 @@
+import { AxiosError } from 'axios'
+import { useQuery } from 'react-query'
+import { api } from '../api'
+
+type User = {
+  id: string
+  name: string
+  email: string
+  createdAt: string
+}
+
+type Response = {
+  users: User[]
+}
+
+async function getUsers(): Promise<User[]> {
+  const { data } = await api.get<Response>('users')
+
+  const users = data.users.map((user) => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
+
+  return users
+}
+
+export function useUsers() {
+  return useQuery<User[], AxiosError>('users', getUsers)
+}
